4309bc1e66e4c8c936ff43ac88ea2fa4e390e830
isa <sumerisa5@gmail.com>
2021-08-10 00:49:24 +0300

    callback function added

---
+++ callback.js
@@ -0,0 +1,15 @@
+// setTimeout( () => {
+//     console.log("merhaba")
+// }, 3000)
+
+// setInterval (() => {
+//     console.log("ben her saniye çalışacağım")
+// }, 1000)
+
+const sayHi = () => {
+    console.log("Merhaba De")
+};
+
+sayHi(() => {
+    console.log("hello");
+});
