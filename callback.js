be58d7838fbc20b466b998fe34238d33a5f89f8c
isa <sumerisa5@gmail.com>
2021-08-10 09:06:22 +0300

    callback function added

--- callback.js
+++ callback.js
@@ -6,10 +6,16 @@
 //     console.log("ben her saniye çalışacağım")
 // }, 1000)
 
-const sayHi = () => {
-    console.log("Merhaba De")
-};
+// const sayHi = (cb) => {
+//     cb();
+// };
 
-sayHi(() => {
-    console.log("hello");
+// sayHi(() => {
+//     console.log("hello");
+// });
+
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then((data) => {
+    console.log(data)
 });
